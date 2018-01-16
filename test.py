a439f52cdbb09ff17020e3dc8f1ab97865e9f0e4
RobDHess <HesselinkRD@gmail.com>
2018-01-16 14:25:38 +0100

    added collapse time

--- test.py
+++ test.py
@@ -5,20 +5,10 @@ Created on Thu Jan 11 17:14:56 2018
 @author: Larry
 """
 
-class p():
-    def __init__(self, p):
-        self.p = p
-        
-    def __str__(self):
-        return 'pwep'
-    def __cmp(self):
-        return 2
-    def __float__(self):
-        return 2.2
-    def __add__(self, b):
-        return self.p + 3
-    
-a = p(2)
+x = 3
+y = "3"
 
-print (a)
-print (a + 2)
+y += " dit er nog bij"
+y = "potato"
+
+print (y)
